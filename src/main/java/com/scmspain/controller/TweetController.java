4c41c447bcec8cfcb88c5e5e908603479c87b0ba
Ricard Moré <ricard.more@axpm.com>
2017-07-19 21:40:39 +0200

    Javadoc

--- src/main/java/com/scmspain/controller/TweetController.java
+++ src/main/java/com/scmspain/controller/TweetController.java
@@ -7,7 +7,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
@@ -33,21 +32,23 @@ public class TweetController {
         this.tweetService = tweetService;
     }
 
-    @ApiOperation(value = "Reads a paged list of tweets sorted by publication date desc", httpMethod = "GET")
+    @ApiOperation(value = "Reads a paged list of tweets sorted by publication date desc. This method accepts pageable parameters: page and size.", httpMethod = "GET")
     @GetMapping(path = "/tweet", produces = APPLICATION_JSON_UTF8_VALUE)
     @ResponseStatus(OK)
-    public List<TweetDTO> listAllTweets(@PageableDefault(value = 20, page = 0) Pageable pageable) {
-        return this.tweetService.listAllTweets(pageable).getContent().stream().map(t -> this.conversionService.convert(t, TweetDTO.class)).collect(Collectors.toList());
+    public List<TweetDTO> listAllTweets(@PageableDefault(size = 100, page = 0) Pageable pageable) {
+        return this.tweetService.listAllTweets(pageable).getContent().stream().map(
+                t -> this.conversionService.convert(t, TweetDTO.class)
+        ).collect(Collectors.toList());
     }
 
-    @ApiOperation(value = "Publishes and stotes a new tweet", httpMethod = "POST")
+    @ApiOperation(value = "Publishes and stores a new tweet", httpMethod = "POST")
     @PostMapping(path = "/tweet", consumes = APPLICATION_JSON_UTF8_VALUE)
     @ResponseStatus(CREATED)
     public void publishTweet(@RequestBody PublishTweetCommand publishTweet) throws InvalidArgumentException {
         this.tweetService.publishTweet(publishTweet.getPublisher(), publishTweet.getTweet());
     }
 
-    @ApiOperation(value = "Discard a tweet", httpMethod = "POST")
+    @ApiOperation(value = "Discards a tweet", httpMethod = "POST")
     @PostMapping(path = "/discarded", consumes = APPLICATION_JSON_UTF8_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public void discardTweet(@RequestBody TweetId discardedTweet) {
@@ -57,8 +58,10 @@ public class TweetController {
     @ApiOperation(value = "Reads a paged list of discarded tweets sorted by discartion date desc", httpMethod = "GET")
     @GetMapping(path = "/discarded", produces = APPLICATION_JSON_UTF8_VALUE)
     @ResponseStatus(OK)
-    public Page<TweetDTO> listDiscardedTweets(@PageableDefault(value = 20, page = 0) Pageable pageable) {
-        return this.tweetService.listDiscardedTweets(pageable).map(t -> this.conversionService.convert(t, TweetDTO.class));
+    public List<TweetDTO> listDiscardedTweets(@PageableDefault(size = 100, page = 0) Pageable pageable) {
+        return this.tweetService.listDiscardedTweets(pageable).getContent().stream().map(
+                t -> this.conversionService.convert(t, TweetDTO.class)
+        ).collect(Collectors.toList());
     }
 
 
